067b80ad98e60be2d4aeb603d47b98bf7e94b013
Padl3xx <60220773+Padl3xx@users.noreply.github.com>
2023-08-04 09:34:06 +0200

    Update install_agent2_template.ps1

--- install_agent2_template.ps1
+++ install_agent2_template.ps1
@@ -4,6 +4,7 @@
 #
 ##The script starts the GPO when the computer starts, it is applied to computers, not users.
 ##Create a new GPO, go to, Computer Configuration -> Policies -> Windows Settings -> Scripts -> Startup -> Powershell Scripts -> Add.
+##Dont forget to link the GPO on specified OU :-)
 ##It should probably work if you run the installation directly from your computer, but I haven't tried that.
 #
 # Construct the FQDN (Fully Qualified Domain Name) of the local computer
